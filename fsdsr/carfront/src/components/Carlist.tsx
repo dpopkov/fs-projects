2498594ea25ebaeb6ad7e92965cfc382600489b4
Denis Popkov <pkvdenis@gmail.com>
2023-09-30 21:32:51 +0300

    carfront: implement deleting car.

--- fsdsr/carfront/src/components/Carlist.tsx
+++ fsdsr/carfront/src/components/Carlist.tsx
@@ -1,11 +1,22 @@
-import { useQuery } from '@tanstack/react-query';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
-import { getCars } from '../api/carapi';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import { DataGrid, GridColDef, GridCellParams } from '@mui/x-data-grid';
+import { getCars, deleteCar } from '../api/carapi';
 
 function Carlist() {
+  const queryClient = useQueryClient();
+
   const { data, error, isSuccess } = useQuery({
     queryKey: ['cars'],
     queryFn: getCars,
+  });
+
+  const { mutate } = useMutation(deleteCar, {
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['cars'] });
+    },
+    onError: (err) => {
+      console.error(err);
+    },
   });
 
   const columns: GridColDef[] = [
@@ -15,6 +26,19 @@ function Carlist() {
     { field: 'registrationNumber', headerName: 'Reg.nr.', width: 150 },
     { field: 'modelYear', headerName: 'Model Year', width: 150 },
     { field: 'price', headerName: 'Price', width: 150 },
+    {
+      field: 'delete',
+      headerName: '',
+      width: 90,
+      sortable: false,
+      filterable: false,
+      disableColumnMenu: true,
+      renderCell: (params: GridCellParams) => (
+        <button onClick={() => mutate(params.row._links.car.href)}>
+          Delete
+        </button>
+      ),
+    },
   ];
 
   if (!isSuccess) {
@@ -26,6 +50,7 @@ function Carlist() {
       <DataGrid
         rows={data}
         columns={columns}
+        disableRowSelectionOnClick={true}
         getRowId={(row) => row._links.self.href}
       />
     );
