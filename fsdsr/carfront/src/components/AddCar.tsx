29f1bf46607d9f07bb68de7bb7f196e34ef89baf
Denis Popkov <pkvdenis@gmail.com>
2023-10-17 21:38:14 +0300

    carfront: refactor dialog fields into CarDialogContent.

--- fsdsr/carfront/src/components/AddCar.tsx
+++ fsdsr/carfront/src/components/AddCar.tsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 import { Dialog } from '@mui/material';
 import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { Car } from '../types';
 import { addCar } from '../api/carapi';
+import CarDialogContent from './CarDialogContent';
 
 function AddCar() {
   const [open, setOpen] = useState(false);
@@ -56,44 +56,7 @@ function AddCar() {
       <button onClick={handleClickOpen}>New Car</button>
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>New car</DialogTitle>
-        <DialogContent>
-          <input
-            placeholder="Brand"
-            name="brand"
-            value={car.brand}
-            onChange={handleChange}
-          />
-          <input
-            placeholder="Model"
-            name="model"
-            value={car.model}
-            onChange={handleChange}
-          />
-          <input
-            placeholder="Color"
-            name="color"
-            value={car.color}
-            onChange={handleChange}
-          />
-          <input
-            placeholder="Year"
-            name="modelYear"
-            value={car.modelYear}
-            onChange={handleChange}
-          />
-          <input
-            placeholder="Reg.nr"
-            name="registrationNumber"
-            value={car.registrationNumber}
-            onChange={handleChange}
-          />
-          <input
-            placeholder="Price"
-            name="price"
-            value={car.price}
-            onChange={handleChange}
-          />
-        </DialogContent>
+        <CarDialogContent car={car} handleChange={handleChange} />
         <DialogActions>
           <button onClick={handleClose}>Cancel</button>
           <button onClick={handleSave}>Save</button>
