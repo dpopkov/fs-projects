fecae1af71a725d30efbd36e91915eb894d9eed6
Denis Popkov <pkvdenis@gmail.com>
2023-10-22 22:30:18 +0300

    carfront: add token in REST API calls.

--- fsdsr/carfront/src/api/carapi.ts
+++ fsdsr/carfront/src/api/carapi.ts
@@ -2,22 +2,30 @@ import { CarResponse, Car, CarEntry } from '../types';
 import axios from 'axios';
 
 export const getCars = async (): Promise<CarResponse[]> => {
-  const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/cars`);
+  const token = sessionStorage.getItem('jwt');
+  const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/cars`, {
+    headers: { Authorization: token },
+  });
   return response.data._embedded.cars;
 };
 
 export const deleteCar = async (link: string): Promise<CarResponse> => {
-  const response = await axios.delete(link);
+  const token = sessionStorage.getItem('jwt');
+  const response = await axios.delete(link, {
+    headers: { Authorization: token },
+  });
   return response.data;
 };
 
 export const addCar = async (car: Car): Promise<CarResponse> => {
+  const token = sessionStorage.getItem('jwt');
   const response = await axios.post(
     `${import.meta.env.VITE_API_URL}/api/cars`,
     car,
     {
       headers: {
         'Content-Type': 'application/json',
+        Authorization: token,
       },
     }
   );
@@ -26,9 +34,11 @@ export const addCar = async (car: Car): Promise<CarResponse> => {
 
 export const updateCar = async (carEntry: CarEntry): Promise<CarResponse> => {
   console.log('Updating', carEntry);
+  const token = sessionStorage.getItem('jwt');
   const response = await axios.patch(carEntry.url, carEntry.car, {
     headers: {
       'Content-Type': 'application/json',
+      Authorization: token,
     },
   });
   return response.data;
