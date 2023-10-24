ec3ec65533da902b3b0cee6b38a4200bd4913282
Denis Popkov <pkvdenis@gmail.com>
2023-10-24 21:15:21 +0300

    sb3wk: users01: create project.

---
+++ sb3wk/users01/src/main/kotlin/UsersApplication.kt
@@ -0,0 +1,11 @@
+package learn.sb3wk.users
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UsersApplication
+
+fun main(args: Array<String>) {
+	runApplication<UsersApplication>(*args)
+}
---
+++ sb3wk/users01/src/test/kotlin/UsersApplicationTests.kt
@@ -0,0 +1,13 @@
+package learn.sb3wk.users
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
